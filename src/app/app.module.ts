0eb2a221164190ae500d72aa4a16fdc4879095c3
Patapizza <Patapizza05@users.noreply.github.com>
2020-01-26 16:31:33 +0100

    Structure HTML AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {AppComponent} from './app.component';
+import {MatCardModule, MatGridListModule, MatToolbarModule} from '@angular/material';
+import {HeaderComponent} from './components/header/header.component';
 import {CategoryTitleComponent} from './components/category/category-title.component';
 import {BannerComponent} from './components/banner/banner.component';
-import {HeaderComponent} from './components/header/header.component';
 import {FormationsComponent} from './components/formations/formations.component';
 import {SkillsComponent} from './components/skills/skills.component';
 import {HobbiesComponent} from './components/hobbies/hobbies.component';
 import {ExperiencesComponent} from './components/experiences/experiences.component';
 import {ProjectsComponent} from './components/projects/projects.component';
 import {LanguagesComponent} from './components/languages/languages.component';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
@@ -20,17 +23,23 @@ import {LanguagesComponent} from './components/languages/languages.component';
     CategoryTitleComponent,
     BannerComponent,
     FormationsComponent,
+    SkillsComponent,
     HobbiesComponent,
     ExperiencesComponent,
+    ProjectsComponent,
+    LanguagesComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     // Angular Material
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+
 }
