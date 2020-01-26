8f554775b9c4fb64a31e545c0d06a46c1f7d3499
Patapizza <Patapizza05@users.noreply.github.com>
2020-01-26 16:26:13 +0100

    BannerComponent
    BannerComponent
    
    
    MatCardModule (Banner)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HeaderComponent} from './components/header/header.component';
 import {CategoryTitleComponent} from './components/category/category-title.component';
+import {BannerComponent} from './components/banner/banner.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FormationsComponent} from './components/formations/formations.component';
+import {MatCardModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
+    // Internal components
     HeaderComponent,
     CategoryTitleComponent,
     BannerComponent,
-    FormationsComponent,
+    FormationsComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // Angular Material
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
