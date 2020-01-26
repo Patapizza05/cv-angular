59b712775807f627e06a4b566b33ec050a8b6cac
Patapizza <Patapizza05@users.noreply.github.com>
2020-01-26 16:26:18 +0100

    HeaderComponent
    HeaderComponent
    
    
    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './components/header/header.component';
 import {CategoryTitleComponent} from './components/category/category-title.component';
 
 @NgModule({
